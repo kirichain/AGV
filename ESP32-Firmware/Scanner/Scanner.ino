d7a55d915dd22ebe9ea93062d350c1f0f827e29b
Nguyen Thanh Duy Tan <cozmo178212@gmail.com>
2023-01-14 10:37:33 +0700

    Update repo 14/01-10:36.AM

---
+++ ESP32-Firmware/Scanner/Scanner.ino
@@ -0,0 +1,54 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 1; //In seconds
+BLEScan* pBLEScan;
+BLEAdvertisedDevice device;
+float rssi;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      //Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+
+      if (advertisedDevice.haveRSSI()){
+        if (advertisedDevice.getName() == "ESP32 1") {
+          Serial.printf("Rssi: %d \n", (int)advertisedDevice.getRSSI());
+        }
+      }
+      else Serial.printf("\n");   
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(1000);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  //Serial.print("Devices found: ");
+  //Serial.println(foundDevices.getCount());
+  for (int i = 0; i < foundDevices.getCount(); i++) {
+    device = foundDevices.getDevice(i);
+    rssi = device.getRSSI();
+    //Serial.println(rssi);
+  }
+  //Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(100);
+}
