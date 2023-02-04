ff2ae78f21dc7f7bae9bfa7d439f7b8c3e283090
Nguyen Thanh Duy Tan <cozmo178212@gmail.com>
2023-02-04 14:01:51 +0700

    Update repo 04/02-2:01.PM

--- Server/Dashboard/Dashboard/wwwroot/js/site.js
+++ Server/Dashboard/Dashboard/wwwroot/js/site.js
@@ -1,4 +1,55 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    //Init, checking dashboard
+    console.log('Loaded');
+    //Checking camera
+    $('#cameraStreamView').addClass('hide');
+    if ($('#cameraSwitch').is(':checked')) {
+        console.log('Camera ON');
+        $('#cameraIndicator').html('ON');
+    } else {
+        console.log('Camera OFF');
+        $('#cameraIndicator').html('OFF');
+    }
+    //
+    $('#cameraSwitch').on('change', function () {
+        if ($('#cameraSwitch').is(':checked')) {
+            console.log('Camera ON');
+            $('#cameraIndicator').html('ON');
+            $('#cameraStreamView').attr('src', 'https://1e09-203-113-151-208.ap.ngrok.io/stream');
+            $('#cameraStreamView').removeClass('hide');
+        } else {
+            console.log('Camera OFF');
+            $('#cameraIndicator').html('OFF');
+            $('#cameraStreamView').attr('src', '');
+            $('#cameraStreamView').addClass('hide');
+        }
+    });
+    //Checking speed indicator
+    $('#speedIndicator').html('Motor speed: ' + $('#speedSlider').val());
 
-// Write your JavaScript code.
+    document.getElementById('speedSlider').oninput = function () {
+        console.log("Speed Slider = " + $('#speedSlider').val());
+        $('#speedIndicator').html('Motor speed: ' + $('#speedSlider').val());
+    };
+    //
+    //Checking steering assist
+    if ($('#steeringAssistSwitch').is(':checked')) {
+        console.log('Steering Assist ON');
+        $('#steeringAssistIndicator').html('Steering Assist: ON');
+    } else {
+        console.log('Steering Assist OFF');
+        $('#steeringAssistIndicator').html('Steering Assist: OFF');
+    }
+
+    $('#steeringAssistSwitch').on('change', function () {
+        if ($('#steeringAssistSwitch').is(':checked')) {
+            console.log('Steering Assist ON');
+            $('#steeringAssistIndicator').html('Steering Assist: ON');
+        } else {
+            console.log('Steering Assist OFF');
+            $('#steeringAssistIndicator').html('Steering Assist: OFF');
+        }
+    });
+
+
+})
